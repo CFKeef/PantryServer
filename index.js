a4478b6cc035e582fd165d69d05e50f8585ad0bb
CFKeef <Patryck.Golebiewski29@qmail.cuny.edu>
2020-12-29 19:38:48 -0500

    Activating account works!

--- index.js
+++ index.js
@@ -66,8 +66,10 @@ app.get("/activation", (req, res) => {
                     return res.sendStatus(403);
                 }
                 else {
+                    console.log(decoded.exp * 1000);
+                    console.log(Date.now())
                     // Took too long to activate
-                    if(decoded.exp < new Date().now()) return res.sendStatus(403);
+                    if(decoded.exp * 1000 < Date.now()) return res.sendStatus(403);
                     else {
                         pg.updateUserVerifiedFlag(decoded.id);
                         res.sendStatus(200);
